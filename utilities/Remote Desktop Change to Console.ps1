c18be3f2100f2c85db0c145328e7b40e0b9ea0e4
Nathaniel Collier <nathaniel.a.collier@gmail.com>
2022-04-21 17:44:46 -0500

    Found a simpler way
    It still doesn't work right

--- utilities/Remote Desktop Change to Console.ps1
+++ utilities/Remote Desktop Change to Console.ps1
@@ -1,43 +1,16 @@
 
-function run-qwinsta(){
-    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
-    $pinfo.FileName = "cmd.exe"
-    $pinfo.RedirectStandardError = $true
-    $pinfo.RedirectStandardOutput = $true
-    $pinfo.UseShellExecute = $false
-    $pinfo.Arguments = "/C qwinsta"
-    $p = New-Object System.Diagnostics.Process
-    $p.StartInfo = $pinfo
-    $p.Start() | Out-Null
-    $p.WaitForExit()
-    $stdout = $p.StandardOutput.ReadToEnd()
-    $stderr = $p.StandardError.ReadToEnd()
-
-    return [PSCustomObject]@{
-        stdout= $stdout
-        stderror = $stderr
-        exitcode = $p.ExitCode
-    }
-}
-
-function parse-qwinstaOutput(){
+function Get-TSSessions {
     param(
-        [string]
-        $outputText
+        $ComputerName = "localhost"
     )
 
-    $lines = $outputText.Split("`n")
-
-    # first line is header so skip it, then the columns are tab delimited
-    $lines | select -Skip 1 | foreach{
-        $columns = $_.Split(" ") | where { -not [string]::IsNullOrWhiteSpace($_)} | foreach{ $_.Trim() }
-
-    }
+    qwinsta /server:$ComputerName |
+    #Parse output
+    ForEach-Object {
+        $_.Trim() -replace "\s+",","
+    } |
+    #Convert to objects
+    ConvertFrom-Csv
 }
 
-function get-ActiveConsole(){
-    $results = run-qwinsta
-    $entries = parse-qwinstaOutput $results.stdout
-}
-
-get-ActiveConsole
+Get-TSSessions
